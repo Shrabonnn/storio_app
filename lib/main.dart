6ac6d439c0a11c906cfe7e62e5b58dd748c0df9a
shrabonnn <shrabonewmc009@gmail.com>
2026-08-12 13:09:51 +0600

    use container to deploy on vercel app

--- lib/main.dart
+++ lib/main.dart
@@ -19,86 +19,104 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Sizer(builder: (context,orientation,screenType){
-      return Container(
-        width: 412,
-        height: 915,
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
 
-          localizationsDelegates: const [
-            FlutterQuillLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-
-          supportedLocales: const [
-            Locale('en'),
-          ],
-
-
-          initialRoute: RoutesName.splash_screen,
-          onGenerateRoute: Routes.generateRoute,
-          theme: ThemeData(
-              textTheme: GoogleFonts.interTextTheme(
-                ThemeData.light().textTheme,
-              ).apply(
-                bodyColor: AppColors.primary,
-                displayColor: AppColors.primary
-              ),
-
-              inputDecorationTheme: InputDecorationTheme(
-                filled: true,
-                fillColor: Colors.white70,
-
-                hintStyle: const TextStyle(color: Colors.black38, fontSize: 14),
-
-                contentPadding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 12,
-                ),
-
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: AppColors.secondary, width: 1),
-                ),
-
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: AppColors.secondary, width: 1.5),
-                ),
-
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.red, width: 1),
-                ),
-
-                focusedErrorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.red, width: 1.5),
-                ),
-
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-
-              elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primary,
-                  foregroundColor: Colors.white,
-                  minimumSize: const Size.fromHeight(48),
-                  //fixedSize: Size.fromWidth(double.maxFinite),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(AppSizes.buttonRadius),
+        //add
+        builder: (context, child) {
+          return Center(
+            child: Container(
+              width: 412,
+              height: 915,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 20,
+                    spreadRadius: 2,
                   ),
-                  textStyle: TextStyle(
-                    fontSize: AppSizes.sectionTitle,
-                    fontWeight: FontWeight.w600,
-                  ),
+                ],
+              ),
+              child: child!,
+            ),
+          );
+        },
+
+        localizationsDelegates: const [
+          FlutterQuillLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+
+        supportedLocales: const [
+          Locale('en'),
+        ],
+
+
+        initialRoute: RoutesName.splash_screen,
+        onGenerateRoute: Routes.generateRoute,
+        theme: ThemeData(
+            textTheme: GoogleFonts.interTextTheme(
+              ThemeData.light().textTheme,
+            ).apply(
+              bodyColor: AppColors.primary,
+              displayColor: AppColors.primary
+            ),
+
+            inputDecorationTheme: InputDecorationTheme(
+              filled: true,
+              fillColor: Colors.white70,
+
+              hintStyle: const TextStyle(color: Colors.black38, fontSize: 14),
+
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 12,
+              ),
+
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: AppColors.secondary, width: 1),
+              ),
+
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: AppColors.secondary, width: 1.5),
+              ),
+
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.red, width: 1),
+              ),
+
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.red, width: 1.5),
+              ),
+
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primary,
+                foregroundColor: Colors.white,
+                minimumSize: const Size.fromHeight(48),
+                //fixedSize: Size.fromWidth(double.maxFinite),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(AppSizes.buttonRadius),
+                ),
+                textStyle: TextStyle(
+                  fontSize: AppSizes.sectionTitle,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            scaffoldBackgroundColor: AppColors.background
+            ),
+          scaffoldBackgroundColor: AppColors.background
 
-          ),
         ),
       );
     });
